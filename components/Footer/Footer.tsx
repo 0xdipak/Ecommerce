61160aeb52957cd8aaf62460383e9f414d3d784f
k1r03478 <dipaksharma34447255@gmail.com>
2024-01-16 14:00:03 +0545

    Project setup

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center bg-base-300 p-4 text-base-content">
+      <p>Copyright &copy; 2024 - All right reserved by E-Commerce</p>
+    </footer>
+  );
+};
+
+export default Footer;
